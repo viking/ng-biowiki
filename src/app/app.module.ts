d39ceab21cbd6e69900b50d36a0b96085f024339
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-08-18 10:10:26 -0500

    Add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { WebService } from './web.service';
@@ -18,6 +19,7 @@ import { MarkdownPipe } from './markdown.pipe';
   imports: [
     BrowserModule,
     HttpModule,
+    NgbModule.forRoot(),
     AppRoutingModule,
   ],
   providers: [WebService],
